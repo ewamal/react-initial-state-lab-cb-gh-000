a1774357552b2d46be97bfe25256e4457e6456b7
Ewa Malusecka <maluseckae@gmail.com>
2019-02-15 09:13:33 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -11,9 +11,16 @@ export default class ImageSlider extends React.Component {
 
   render(){
     let CURRENT_SLIDE = this.state.currentSlideIndex;
+<<<<<<< HEAD
 
     return(
       <div>I am on slide {CURRENT_SLIDE}</div>
+=======
+    let temp = (CURRENT_SLIDE > 0) ? `I am on slide ${CURRENT_SLIDE}` : 'Boom!'
+
+    return(
+      <div>{temp}</div>
+>>>>>>> 6af05e9b627b103a85a6e53f474ffbc53e843ec7
     )
   }
 }
